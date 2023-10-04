4d1bdf5d82f1ed4fe119867071fa095cc2565282
Ethan Wells <ethanmw2000@gmail.com>
2023-10-03 23:20:02 -0500

    adding file selection component

---
+++ app/_components/FileSelection.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+export default function FileSelection() {
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0];
+    if (selectedFile) {
+      console.log(selectedFile);
+    }
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const selectedFile = e.dataTransfer.files?.[0];
+    if (selectedFile) {
+      console.log(selectedFile);
+    }
+  };
+
+  return (
+    <div
+      onDrop={handleDrop}
+      onDragOver={(e) => e.preventDefault()}
+      className="flex flex-row max-w-2xl h-64 justify-center items-center border-2 border-gray-300 border-dashed rounded-lg hover:border-gray-400 transition-colors"
+    >
+      <div className="flex flex-col items-center">
+      <p>
+        <b>Drag and drop</b> or <b>select</b> a file to upload
+      </p>
+      <input
+        type="file"
+        className="file-input file-input-bordered w-full max-w-xs"
+        onChange={handleFileChange}
+      />
+      </div>
+    </div>
+  );
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { Canvas, useLoader } from "@react-three/fiber";
 import { PLYLoader } from "three/examples/jsm/loaders/PLYLoader";
+import FileSelection from "./_components/FileSelection";
 
 export default function Home() {
   const ply = useLoader(PLYLoader, "point_cloud.ply");
@@ -11,6 +12,7 @@ export default function Home() {
     <div>
       <p>Hello</p>
       <button className="btn btn-primary">Click me</button>
+      <FileSelection />
       <div className="border">
         <Canvas>
           <ambientLight intensity={0.1} />
