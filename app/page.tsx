bd25362afb07a996d71d37bb97742f200d75d63c
Ethan Wells <ethanmw2000@gmail.com>
2023-10-05 14:28:05 -0500

    3d models working for pcd

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,41 @@
-'use client';
+"use client";
 
-import { Canvas, useLoader } from '@react-three/fiber';
-import { PLYLoader } from 'three/examples/jsm/loaders/PLYLoader.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import FileSelection from './_components/FileSelection';
-import { Suspense, useEffect, useState } from 'react';
-import { PerspectiveCamera } from 'three';
-import { PivotControls } from '@react-three/drei';
+import { Canvas, useLoader } from "@react-three/fiber";
+import FileSelection from "./_components/FileSelection";
+import { Suspense, useEffect, useState } from "react";
+import { OrbitControls, Point } from "@react-three/drei";
+import { PCDLoader } from "three/examples/jsm/loaders/PCDLoader.js";
+import THREE, { Vector3, Box3 } from "three";
 
 export default function Home() {
   const [file, setFile] = useState<File>();
   // const ply = useLoader(PLYLoader, file?.name as string);
-  const gltf = useLoader(PLYLoader, '/point_cloud.gltf');
+  // const gltf = useLoader(GLTFLoader, '/point_cloud.gltf');
+  const pcd = useLoader(PCDLoader, "/point_cloud.pcd");
+  const [center, setCenter] = useState<Vector3>();
+
+  useEffect(() => {
+    let box = new Box3().setFromObject(pcd);
+    let newCenter = box.getCenter(new Vector3());
+    setCenter(newCenter);
+  }, [pcd]);
 
   return (
     <div>
-      <p>Hello</p>
+      {/* <p>Hello</p>
       <button className="btn btn-primary">Click me</button>
-      <FileSelection setFile={setFile} />
+      <FileSelection setFile={setFile} /> */}
       <div className="border">
-        <Canvas style={{ height: '100vh', width: '100vw' }} shadows camera={{ position: [-3, 0.5, 3] }}>
-          <Suspense>
-            {/* <pointLight color="#f6f3ea" position={[10, 5, 10]} intensity={2} /> */}
-            <PivotControls anchor={[-1.1, -1.1, -1.1]} scale={0.75} lineWidth={3.5}>
-            <primitive scale={[0.25, 0.25, 0.25]} object={gltf.scene} />
-            </PivotControls>
-          </Suspense>
+        <Canvas
+          style={{ height: "100vh", width: "100vw" }}
+          shadows
+          camera={{
+            position: [center?.x ?? 0, center?.y ?? 0, center?.z ?? 0],
+          }}
+        >
+          {/* <pointLight color="#f6f3ea" position={[10, 5, 10]} intensity={2} /> */}
+            <primitive object={pcd} scale={[0.25, 0.25, 0.25]} />
+          <OrbitControls />
         </Canvas>
       </div>
     </div>
