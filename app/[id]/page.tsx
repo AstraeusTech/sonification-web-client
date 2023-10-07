c19787dbdc5acd478b851e4d81fc58f5f4721ec1
Ethan Wells <ethanmw2000@gmail.com>
2023-10-07 11:52:51 -0500

    branch changes

---
+++ app/[id]/page.tsx
@@ -0,0 +1,7 @@
+import ModelViewer from "../_components/ModelViewer";
+
+export default function Sonification() {
+  return (
+    <ModelViewer />
+  )
+}
--- app/_components/ModelViewer.tsx
+++ app/_components/ModelViewer.tsx
@@ -1,34 +1,35 @@
 "use client";
 
+import { Center, OrbitControls, OrthographicCamera } from "@react-three/drei";
 import { Canvas, useLoader } from "@react-three/fiber";
 import { useEffect, useState } from "react";
-import { OrbitControls, Center } from "@react-three/drei";
+import { Box3, Vector3 } from "three";
 import { PCDLoader } from "three/examples/jsm/loaders/PCDLoader.js";
-import { Vector3, Box3 } from "three";
 
-export default function ModelViewer() {
-// const pcd = useLoader(PCDLoader, file?.name as string);
-const pcd = useLoader(PCDLoader, "/point_cloud.pcd");
-const [size, setSize] = useState<Vector3>();
+interface ModelViewerProps {
+  file?: File;
+}
 
-useEffect(() => {
-  let box = new Box3().setFromObject(pcd);
-  let size = box.getSize(new Vector3());
-  setSize(size);
-}, [pcd]);
+export default function ModelViewer(props: ModelViewerProps) {
+  // const pcd = useLoader(PCDLoader, file?.name as string);
+  const pcd = useLoader(PCDLoader, "/point_cloud.pcd");
+  const [size, setSize] = useState<Vector3>();
+
+  useEffect(() => {
+    let box = new Box3().setFromObject(pcd);
+    let size = box.getSize(new Vector3());
+    setSize(size);
+  }, [pcd]);
 
   return (
-    <Canvas
-      style={{ height: "100vh", width: "100vw" }}
-      shadows
-      camera={{
-        position: [(size?.x || 0)/4, 0, (size?.z || 0)],
-      }}
-    >
-      <ambientLight intensity={0.5} />
+    <Canvas style={{ height: "100vh", width: "100vw" }} shadows>
       <Center>
-        <primitive object={pcd} scale={[0.25, 0.25, 0.25]} />
+        <primitive object={pcd} scale={[0.5, 0.5, 0.5]} />
       </Center>
+      <OrthographicCamera
+        makeDefault
+        position={[(size?.x || 0) / 4, 0, size?.z || 0]}
+      />
       <OrbitControls />
     </Canvas>
   );
