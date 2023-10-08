63ea5565dcd586b27aa6513e9c3424ae4211db02
walter.ownbeyiv <walter.ownbeyiv@invista.com>
2023-10-08 02:47:57 -0500

    new

--- app/_components/ToolOverlay.tsx
+++ app/_components/ToolOverlay.tsx
@@ -6,11 +6,11 @@ interface ToolOverlayProps {
 }
 export default function ToolOverlay(props: ToolOverlayProps): JSX.Element {
   return (
-    <div className="absolute bottom-10 w-full p-5 flex flex-row gap-10 bg-indigo-100/50 justify-center items-center rounded-lg  ">
-      <div className="w-screen ">
+    <div className="absolute bottom-10 w-full p-5 flex flex-row gap-10 justify-center items-center rounded-lg  ">
+      <div className="w-screen pl-10 ">
         <WaveSurferPlayer
           height={100}
-          waveColor="rgb(0, 0, 0)"
+          waveColor="rgb(125, 25, 25)"
           progressColor="rgb(255, 0, 0)"
           url={"/sound-combined.wav"}
           container={""}
@@ -28,7 +28,7 @@ export default function ToolOverlay(props: ToolOverlayProps): JSX.Element {
           props.view.current = obj.target.value;
         }}
         defaultValue={"select"}
-        className="select text-lg max-w-xs bg-black text-white h-full"
+        className="select text-lg max-w-xs bg-black text-white h-full pr-10"
       >
         <option value={"select"} disabled>
           Select view
