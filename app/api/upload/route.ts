570805f929d9a558aa166a11af6749e8b4d87546
Mirmukhammad Mirsodikov <49703214+mir-mirsodikov@users.noreply.github.com>
2023-10-08 21:48:59 -0500

    Added functionality to upload file to the sonification api and redirect the user to the new page

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,11 +1,11 @@
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
-const client = new S3Client({ region: process.env.AWS_DEFAULT_REGION});
+const client = new S3Client({ region: process.env.AWS_DEFAULT_REGION });
 const bucket = process.env.S3_BUCKET;
 
 import { v4 as uuidv4 } from 'uuid';
 
-async function uploadFile(buffer: Buffer) {
+async function uploadFile(buffer: Buffer): Promise<string> {
   const uuid = uuidv4();
   const uploadCommand = new PutObjectCommand({
     Bucket: bucket,
@@ -15,21 +15,50 @@ async function uploadFile(buffer: Buffer) {
 
   try {
     await client.send(uploadCommand);
+
+    console.log('uploaded file to s3');
+    console.log('attempting to call python api');
+
+    console.log(
+      'process.env.SONIFICATION_API_URL: ',
+      process.env.SONIFICATION_API_URL,
+    );
+
+    const res = await fetch(`${process.env.SONIFICATION_API_URL}/${uuid}.png`, {
+      headers: {
+        contentType: 'application/json',
+      },
+    });
+
+    // console.log('res: ', res);
+
+    if (!res.ok) {
+      throw new Error('Network response was not ok');
+    }
+    console.log('response was ok');
+    return uuid;
   } catch (err: any) {
     if (err.code === 'EntityAlreadyExists') {
-      console.error(`File with key ${uuid} already exists. Retrying with new UUID.`);
+      console.error(
+        `File with key ${uuid} already exists. Retrying with new UUID.`,
+      );
       await uploadFile(buffer);
     } else {
       console.error(err);
     }
   }
+
+  return '';
 }
 
 export async function POST(request: Request) {
   const buffer = await request.arrayBuffer();
 
-  await uploadFile(Buffer.from(buffer));
-  
-  return new Response("File uploaded successfully");
-}
+  const fileName = await uploadFile(Buffer.from(buffer));
 
+  return new Response(
+    JSON.stringify({
+      id: fileName,
+    }),
+  );
+}
