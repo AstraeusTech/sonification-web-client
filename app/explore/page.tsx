fe03724892d19b64415a43d7849eec5881dd3301
Ethan Wells <ethanmw2000@gmail.com>
2023-10-08 22:09:23 -0500

    moved fetch from route handler to server side component

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -6,24 +6,65 @@ interface Thumbnail {
   fileName: string;
 }
 
+import {
+  GetObjectCommand,
+  ListObjectsV2Command,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const client = new S3Client({ region: process.env.AWS_DEFAULT_REGION });
+const bucket = process.env.S3_BUCKET;
+
 async function getThumbnails() {
-  const data = await fetch(`${process.env.API_URL}/api/thumbnails`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    next: {
-      revalidate: 3600,
-    },
-  })
-    .then((res) => res.json())
-    .catch((err) => {
-      console.error(err);
-      return [];
+  const allObjects = [];
+  let continuationToken;
+
+  do {
+    const params: ListObjectsV2Command = new ListObjectsV2Command({
+      Bucket: bucket,
+      ContinuationToken: continuationToken,
     });
 
-  return data;
+    const response = await client.send(params);
+    const objects = response.Contents || [];
+    allObjects.push(...objects);
+    continuationToken = response.NextContinuationToken;
+  } while (continuationToken);
+
+  const recentImages = allObjects
+    .filter(
+      (obj) => obj.LastModified !== undefined && obj.Key?.includes(".png")
+    )
+    .sort((a, b) => {
+      if (a.LastModified && b.LastModified) {
+        return b.LastModified.getTime() - a.LastModified.getTime();
+      }
+      return 0;
+    })
+    .slice(0, 20);
+
+  const images = await Promise.all(
+    recentImages.map(async (obj) => {
+      const params: GetObjectCommand = new GetObjectCommand({
+        Bucket: bucket,
+        Key: obj.Key,
+      });
+
+      const url = await getSignedUrl(client, params, {
+        expiresIn: 3600,
+      });
+
+      return {
+        url: url,
+        fileName: obj.Key?.split(".")[0] as string,
+      };
+    })
+  );
+
+  return images
 }
+
 
 export default async function Explore() {
   const data: Thumbnail[] = await getThumbnails();
